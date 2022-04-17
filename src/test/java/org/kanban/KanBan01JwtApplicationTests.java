ade5181593ba3132baf1bac2ef152c69bf3786e2
CAMILO LEAL <96742005+camiloleal06@users.noreply.github.com>
2022-04-17 16:54:53 -0500

    Update KanBan01JwtApplicationTests.java

--- src/test/java/org/kanban/KanBan01JwtApplicationTests.java
+++ src/test/java/org/kanban/KanBan01JwtApplicationTests.java
@@ -2,8 +2,6 @@ package org.kanban;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class KanBan01JwtApplicationTests {
 
 	@Test
